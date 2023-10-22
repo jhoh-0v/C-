4ccc18c4b4e85f68c87c9b56f00e1e556bcaf61a
jhoh_0v <playshock15@gmail.com>
2023-10-23 07:44:24 +0900

    new file

---
+++ 20230921_practice_1/20230921_practice_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230921_practice_1{
+
+    class Voing_Eligibillty_Checker{
+
+        static void Main(string[] args){
+
+            Console.WriteLine("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if(age >= 18){
+                Console.WriteLine("You eligible to vote.");
+            }
+
+            if(age < 18){
+                Console.WriteLine("You NOT eligible to vote.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
